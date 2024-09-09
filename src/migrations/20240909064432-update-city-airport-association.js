ffb86f90b5924de53d2124e0575d41e633de7ec2
coderbanda-22 <sudarshanbankar22@gmail.com>
2024-09-09 12:59:37 +0530

    Added new migration file specifying the association between airports and cities... also made the required changes in the model files

---
+++ src/migrations/20240909064432-update-city-airport-association.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addConstraint('airports', {
+        fields: ['cityId'],
+        type: 'foreign key',
+        name: 'airports_cityId_fkey',
+        references: {
+            table: 'cities',
+            field: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeConstraint('airports', 'airports_cityId_fkey', )
+  }
+};
